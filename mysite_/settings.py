9587ad2b649dfb2c4b910f3efcb2e906f6120a89
Emirsvgl07 <emirsevgili07@gmail.com>
2026-03-24 11:35:12 +0300

    Üretim ayarları: Gunicorn ve WhiteNoise eklendi

--- mysite_/settings.py
+++ mysite_/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j87hwp1^rlb5(wntu(d-lss-2lrbplqi81w*0_#^mfsuz0_)wg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anket-hub.com', 'www.anket-hub.com', 'ankethub.onrender.com']
 
 
 # Application definition
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',  # 🌟 EKSİKTİ EKLENDİ: Kullanıcının dilini tespit eden dedektif!
     'django.middleware.common.CommonMiddleware',
